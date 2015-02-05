51b4e4fe19b4ca9f6709fc67a64a3a8daef3cbcd
luo.shuidong <lsd751@qq.com>
2015-02-05 18:18:47 +0800

    add simple test script

---
+++ Assets/Script/asse/Util.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Util
+{
+    public static string getGreenString(string info)
+    {
+        return "<color=green>" + info + "</color>";
+    }
+
+    public static void function1()
+    {
+        Debug.Log(getGreenString("@debug: f1 called."));
+    }
+}
---
+++ Assets/Script/mono/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("@debug: start.");
+        Util.function1();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
